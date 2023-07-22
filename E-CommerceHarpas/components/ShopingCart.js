dd17be52b0f6821161d57158d272ae53e6c50f2b
MuhamadAgus <113649505+MhmdAgus21@users.noreply.github.com>
2023-07-22 20:35:07 +0700

    Add files via upload

---
+++ E-CommerceHarpas/components/ShopingCart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, Text, View, Image, StyleSheet, TextInput, } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const ShopingCart = () => {
+    return (
+        <SafeAreaView style={{marginTop:100,marginLeft:50}}>
+            <Text>Ini adalah service</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default ShopingCart;
